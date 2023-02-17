8e0c6de7a0f68a8c5058822dac2a3b3e5b491d97
Catherine Lee <csl@fb.com>
2023-02-17 11:45:35 -0800

    uninstall xdist

--- test/run_test.py
+++ test/run_test.py
@@ -1308,6 +1308,7 @@ def run_test_module(test: Union[str, ShardedTest], test_directory: str, options)
 
 def main():
     options = parse_args()
+    shell([sys.executable, "-m", "pip", "uninstall", "pytest-xdist"])
 
     test_directory = str(REPO_ROOT / "test")
     selected_tests = get_selected_tests(options)
