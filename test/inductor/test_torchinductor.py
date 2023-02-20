f2131a4cb9ea24950901b17d395d4bd97a97d25d
blzheng <beilei.zheng@intel.com>
2023-02-20 00:05:39 -0800

    Inductor: fix crash when indexing an empty tensor by invalid index
    ghstack-source-id: 98fc284d0db3116f0a1c40f88b8a04818d4329bc
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/95046

--- test/inductor/test_torchinductor.py
+++ test/inductor/test_torchinductor.py
@@ -6472,6 +6472,15 @@ if HAS_CPU:
             same(fn(x), opt_fn(x))
             assert metrics.generated_cpp_vec_kernel_count == 0
 
+        def test_invalid_index_of_empty_tensor(self):
+            def fn(a):
+                b = a[[0]]
+                return b
+
+            a = torch.tensor([])
+            with self.assertRaises(RuntimeError):
+                torch.compile(fn)(a)
+
 
 if HAS_CUDA and not TEST_WITH_ASAN:
     import triton
